b99fd7bc0d320d45e62a3d0c5180308989a76bf7
Ngo-Nhut-Nam <nhutnam7114@gmail.com>
2025-08-28 22:18:51 +0700

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $host = "localhost";    // tên server
 $user = "root";         // user mysql
 $pass = "";             // mật khẩu mysql
-$db   = "user_db1";     // tên database của bạn
+$db   = "user_db";     // tên database của bạn
 
 $conn = new mysqli($host, $user, $pass, $db);
 
@@ -13,3 +13,4 @@ if ($conn->connect_error) {
 // Đặt charset utf8 để tránh lỗi tiếng Việt
 $conn->set_charset("utf8");
 ?>
+
