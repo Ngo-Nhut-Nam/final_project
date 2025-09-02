ab351265aa90d32e4b332af964d39f8062c4e548
Ngo-Nhut-Nam <nhutnam7114@gmail.com>
2025-09-02 20:26:47 +0700

    Delete chucnang directory

--- chucnang/js/profile.js
+++
@@ -1,7 +0,0 @@
-function loadProfileData() {
-    const data = JSON.parse(localStorage.getItem('userProfile'));
-    if (!data) return;
-    document.getElementById('profile-email').value = data.email || '';
-    document.getElementById('profile-phone').value = data.phone || '';
-    document.getElementById('profile-password').value = data.password || '';
-}
